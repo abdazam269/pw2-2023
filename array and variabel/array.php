15be524b54b343fd3db0f89c33c479e941551f46
abdazam269 <abdullahazzam79310@gmail.com>
2024-03-10 21:40:57 +0700

    upload

---
+++ array and variabel/array.php
@@ -0,0 +1,10 @@
+<?php
+$hewan = ["kucing","kuda","kelinci","kangguru","koala","keledai"]; 
+echo $hewan[0] . "<br>";
+echo $hewan[5] . "<br>";
+echo "<br>";
+foreach ($hewan as $key => $value) {
+    echo $value . "<br>";
+}
+$hewan[2] = "kukang"; // re-assign / mengerti nilai aray
+echo $hewan[2];
